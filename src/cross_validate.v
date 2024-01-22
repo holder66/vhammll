0783690e3e8ee67007446013a3e7b37a1a782a66
Henry Olders <henry@olders.ca>
2024-01-22 12:28:10 -0500

    removed functions with mut arguments

--- src/cross_validate.v
+++ src/cross_validate.v
@@ -318,8 +318,16 @@ fn do_one_fold(pick_list []int, current_fold int, folds int, ds Dataset, cross_o
 		if disp.verbose_flag {
 			println(y('current fold: ${current_fold}'))
 		}
-		fold_result = classify_in_cross(part_cl, fold_cases, mut fold_result, cross_opts,
-			disp)
+		// fold_result = classify_in_cross(part_cl, fold_cases, mut fold_result, cross_opts, disp)
+
+		for i, case in fold_cases {
+		classify_result := classify_case(part_cl, case, cross_opts, disp)
+		if disp.verbose_flag {
+			verbose_result(i, part_cl, classify_result)
+		}
+		fold_result.inferred_classes << classify_result.inferred_class
+		fold_result.actual_classes << fold_result.labeled_classes[i]
+	}
 	} else { // ie, asking for multiple classifiers...
 		mut classifier_array := []Classifier{}
 		mut instances_to_be_classified := [][][]u8{}
@@ -339,8 +347,17 @@ fn do_one_fold(pick_list []int, current_fold int, folds int, ds Dataset, cross_o
 			m_fold_instances := transpose(byte_values_array)
 			instances_to_be_classified << m_fold_instances
 		}
-		fold_result = multiple_classify_in_cross(current_fold, classifier_array, transpose(instances_to_be_classified), mut
-			fold_result, mult_opts)
+		// fold_result = multiple_classify_in_cross(current_fold, classifier_array, transpose(instances_to_be_classified), mut fold_result, mult_opts)
+		for i, test_instance in transpose(instances_to_be_classified) {
+		// println('i: $i test_instance: $test_instance')
+		m_classify_result := multiple_classifier_classify(classifier_array, test_instance, fold_result.labeled_classes,
+			mult_opts)
+		fold_result.inferred_classes << m_classify_result.inferred_class
+		fold_result.actual_classes << fold_result.labeled_classes[i]
+		fold_result.nearest_neighbors_by_class << m_classify_result.nearest_neighbors_by_class
+	}
+	fold_result.MultipleOptions = mult_opts.MultipleOptions
+	fold_result.MultipleClassifiersArray = mult_opts.MultipleClassifiersArray
 	}
 	// println('fold_result.maximum_hamming_distance: ${fold_result.maximum_hamming_distance}')
 	return fold_result
@@ -372,35 +389,35 @@ fn option_worker(work_channel chan int, result_channel chan CrossVerifyResult, p
 
 // multiple_classify_in_cross classifies each instance in an array, and
 // returns the results of the classification.
-fn multiple_classify_in_cross(fold int, m_cl []Classifier, m_test_instances [][][]u8, mut result CrossVerifyResult, opts Options, disp DisplaySettings) CrossVerifyResult {
-	// println('opts in multiple_classify_in_cross: ${opts}')
-	// println('labeled_classes in multiple_classify_in_cross: ${result.labeled_classes}')
-	// for each instance in the test data, perform a classification
-	for i, test_instance in m_test_instances {
-		// println('i: $i test_instance: $test_instance')
-		m_classify_result := multiple_classifier_classify(m_cl, test_instance, result.labeled_classes,
-			opts)
-		result.inferred_classes << m_classify_result.inferred_class
-		result.actual_classes << result.labeled_classes[i]
-		result.nearest_neighbors_by_class << m_classify_result.nearest_neighbors_by_class
-	}
-	result.MultipleOptions = opts.MultipleOptions
-	result.MultipleClassifiersArray = opts.MultipleClassifiersArray
-	// println('result in multiple_classify_in_cross: ${result}')
-	return result
-}
+// fn multiple_classify_in_cross(fold int, m_cl []Classifier, m_test_instances [][][]u8, mut result CrossVerifyResult, opts Options, disp DisplaySettings) CrossVerifyResult {
+// 	// println('opts in multiple_classify_in_cross: ${opts}')
+// 	// println('labeled_classes in multiple_classify_in_cross: ${result.labeled_classes}')
+// 	// for each instance in the test data, perform a classification
+// 	for i, test_instance in m_test_instances {
+// 		// println('i: $i test_instance: $test_instance')
+// 		m_classify_result := multiple_classifier_classify(m_cl, test_instance, result.labeled_classes,
+// 			opts)
+// 		result.inferred_classes << m_classify_result.inferred_class
+// 		result.actual_classes << result.labeled_classes[i]
+// 		result.nearest_neighbors_by_class << m_classify_result.nearest_neighbors_by_class
+// 	}
+// 	result.MultipleOptions = opts.MultipleOptions
+// 	result.MultipleClassifiersArray = opts.MultipleClassifiersArray
+// 	// println('result in multiple_classify_in_cross: ${result}')
+// 	return result
+// }
 
 // classify_in_cross classifies each case in an array, and
 // returns the results of the classification.
-fn classify_in_cross(cl Classifier, cases [][]u8, mut result CrossVerifyResult, opts Options, disp DisplaySettings) CrossVerifyResult {
-	// for each instance in the test data, perform a classification
-	for i, case in cases {
-		classify_result := classify_case(cl, case, opts, disp)
-		if disp.verbose_flag {
-			verbose_result(i, cl, classify_result)
-		}
-		result.inferred_classes << classify_result.inferred_class
-		result.actual_classes << result.labeled_classes[i]
-	}
-	return result
-}
+// fn classify_in_cross(cl Classifier, cases [][]u8, mut result CrossVerifyResult, opts Options, disp DisplaySettings) CrossVerifyResult {
+// 	// for each instance in the test data, perform a classification
+// 	for i, case in cases {
+// 		classify_result := classify_case(cl, case, opts, disp)
+// 		if disp.verbose_flag {
+// 			verbose_result(i, cl, classify_result)
+// 		}
+// 		result.inferred_classes << classify_result.inferred_class
+// 		result.actual_classes << result.labeled_classes[i]
+// 	}
+// 	return result
+// }
---
+++ src/multi_cross_test.v
@@ -0,0 +1,144 @@
+// multi_cross_test.v
+
+module vhammll
+
+import os
+// import vtl
+
+fn testsuite_begin() ? {
+	if os.is_dir('tempfolder_multi_cross') {
+		os.rmdir_all('tempfolder_multi_cross')!
+	}
+	os.mkdir_all('tempfolder_multi_cross')!
+}
+
+fn testsuite_end() ? {
+	os.rmdir_all('tempfolder_multi_cross')!
+}
+
+
+
+fn test_multiple_crossvalidate() ? {
+	mut opts := Options{
+		break_on_all_flag: true
+		combined_radii_flag: false
+		weighting_flag: false
+		command: 'explore'
+	}
+	mut result := CrossVerifyResult{}
+
+	opts.datafile_path = 'datasets/2_class_developer.tab'
+	opts.settingsfile_path = 'tempfolder_multi_cross/2_class.opts'
+	opts.append_settings_flag = true
+	opts.weight_ranking_flag = true
+	mut ds := load_file(opts.datafile_path)
+	mut er := explore(ds, opts)
+	opts.command = 'cross'
+	opts.number_of_attributes = [3]
+	opts.bins = [1, 3]
+	result = cross_validate(ds, opts)
+	opts.multiple_flag = true
+	opts.multiple_classify_options_file_path = opts.settingsfile_path
+	opts.classifier_indices = [2]
+	assert cross_validate(ds, opts).confusion_matrix_map == {
+		'm': {
+			'm': 8.0
+			'f': 1.0
+		}
+		'f': {
+			'm': 1.0
+			'f': 3.0
+		}
+	}
+	opts.classifier_indices = [3]
+	assert cross_validate(ds, opts).confusion_matrix_map == result.confusion_matrix_map
+	opts.classifier_indices = [2, 3]
+	assert cross_validate(ds, opts).confusion_matrix_map == {
+		'm': {
+			'm': 8.0
+			'f': 1.0
+		}
+		'f': {
+			'm': 1.0
+			'f': 3.0
+		}
+	}
+}
+
+fn test_multiple_crossvalidate_only_discrete_attributes() ? {
+	mut opts := Options{
+		datafile_path: 'datasets/breast-cancer-wisconsin-disc.tab'
+		settingsfile_path: 'tempfolder_multi_cross/breast-cancer-wisconsin-disc.opts'
+		append_settings_flag: true
+		command: 'explore'
+	}
+
+	mut ds := load_file(opts.datafile_path)
+	ft := [false, true]
+	for pf in ft {
+		opts.purge_flag = pf
+		for wr in [false, true] {
+			opts.weight_ranking_flag = wr
+			for w in [false, true] {
+				opts.weighting_flag = w
+				er := explore(ds, opts)
+			}
+		}
+	}
+	display_file(opts.settingsfile_path, opts)
+	opts.append_settings_flag = false
+	opts.command = 'cross'
+	opts.classifier_indices = [3, 4, 6, 14]
+	opts.multiple_classify_options_file_path = opts.settingsfile_path
+	opts.multiple_flag = true
+	// for ci in [[3],[4],[6],[14],[3,4],[3,6],[4,6],[3,4,6],[3,4,6,14]] {
+	for ci in [[3, 11, 4, 5, 6, 14]] {
+		opts.classifier_indices = ci
+		for ma in ft {
+			opts.break_on_all_flag = ma
+			for mc in ft {
+				opts.combined_radii_flag = mc
+				cross_validate(ds, opts)
+			}
+		}
+	}
+	opts.command = 'cross'
+	ds = load_file(opts.datafile_path)
+	opts.number_of_attributes = [7]
+	mut result := cross_validate(ds, opts)
+	opts.multiple_flag = true
+	opts.multiple_classify_options_file_path = opts.settingsfile_path
+	opts.classifier_indices = [2]
+	assert cross_validate(ds, opts).confusion_matrix_map == {
+		'benign':    {
+			'benign':    445.0
+			'malignant': 13.0
+		}
+		'malignant': {
+			'benign':    16.0
+			'malignant': 225.0
+		}
+	}
+	opts.classifier_indices = [3]
+	assert cross_validate(ds, opts).confusion_matrix_map == {
+		'benign':    {
+			'benign':    447.0
+			'malignant': 11.0
+		}
+		'malignant': {
+			'benign':    24.0
+			'malignant': 217.0
+		}
+	}
+	opts.classifier_indices = [2, 3]
+	assert cross_validate(ds, opts).confusion_matrix_map == {
+		'benign':    {
+			'benign':    446.0
+			'malignant': 12.0
+		}
+		'malignant': {
+			'benign':    22.0
+			'malignant': 219.0
+		}
+	}
+}
