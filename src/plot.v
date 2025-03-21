7e328da87a9a4a2e1d724f92027f5443a7e2c962
Henry Olders <henry@olders.ca>
2025-03-21 18:01:47 -0400

    improved help and documentation for overfitting stuff

--- src/plot.v
+++ src/plot.v
@@ -17,18 +17,20 @@ mut:
 // number of hits per bin number,over a range of bin numbers,
 // for a continuous attribute, with separate curves for each class.
 fn plot_hits(classes_info Class, attr RankedAttribute, weighting bool) {
-	mut anno1_text := 'Rank value: ${attr.rank_value: -6.2f} at ${attr.bins} bins'
+	mut anno1_text := 'Rank value: ${attr.rank_value:-6.2f} at ${attr.bins} bins'
 	mut annotation1 := plot.Annotation{
-		x: 0
-		text: anno1_text
+		x:     0
+		text:  anno1_text
 		align: 'left'
 	}
 	for hits_array in attr.array_of_hits_arrays {
 		mut plt := plot.Plot.new()
 		for i, class in classes_info.classes {
 			y := if weighting {
-				hits_array[i].map(f64(it)/classes_info.class_counts.values()[i]*100)
-				} else  {hits_array[i].map(f64(it))}
+				hits_array[i].map(f64(it) / classes_info.class_counts.values()[i] * 100)
+			} else {
+				hits_array[i].map(f64(it))
+			}
 			cases := classes_info.class_counts[class]
 			plt.scatter(
 				x:    []int{len: hits_array[i].len, init: index}
@@ -38,22 +40,27 @@ fn plot_hits(classes_info Class, attr RankedAttribute, weighting bool) {
 				name: '${class} (${cases})'
 			)
 			annotation1.y = array_max(classes_info.class_counts.values())
-		
 		}
 		plt.layout(
-			title:    if weighting {'Weighted h'} else {'H'} + 'its per bin, per class, for attribute "${attr.attribute_name}"'
-			autosize: false
-			width:    800
-			xaxis:    plot.Axis{
+			title:       if weighting { 'Weighted h' } else { 'H' } +
+				'its per bin, per class, for attribute "${attr.attribute_name}"'
+			autosize:    false
+			width:       800
+			xaxis:       plot.Axis{
 				title: plot.AxisTitle{
 					text: 'Bin number (bin 0 is for missing values)'
 				}
 			}
-			yaxis:    plot.Axis{
+			yaxis:       plot.Axis{
 				title: plot.AxisTitle{
-					text: if weighting {'Weighted and normalized n'} else {'N'} +'umber of Hits'
+					text: if weighting { 'Weighted and normalized n' } else { 'N' } +
+						'umber of Hits'
 				}
-				range: [0.0, if weighting {100} else { array_max(classes_info.class_counts.values())}]
+				range: [0.0, if weighting {
+					100
+				} else {
+					array_max(classes_info.class_counts.values())
+				}]
 			}
 			annotations: [annotation1]
 		)
